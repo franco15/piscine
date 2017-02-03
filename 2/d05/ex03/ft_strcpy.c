f5b3fdcb37799d1155e1c255c42a2456d6688748
Luis Franco <luis_fm_95@live.com.mx>
2017-02-03 07:02:16 -0800

    fixed strcpy

--- 2/d05/ex03/ft_strcpy.c
+++ 2/d05/ex03/ft_strcpy.c
@@ -15,10 +15,14 @@ char	*ft_strcpy(char *dest, char *src)
 	int i;
 
 	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
+/*miss the following line, didnt cover if the source string exist*/
+	if (*src)
+	{	
+		while (src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
 	}
 	dest[i] = '\0';
 	return (dest);
